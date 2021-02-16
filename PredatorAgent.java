530da30b12885605f1820279d8e527add5e39dbc
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2021-02-16 17:56:21 +0100

    TP_Agent

--- PredatorAgent.java
+++ PredatorAgent.java
@@ -9,7 +9,7 @@ public class PredatorAgent extends Agent {
 	public PredatorAgent( int __x, int __y, World __w )
 	{
 		super(__x,__y,__w);
-		// energie = seuil;
+		
 
 		// _redValue = 255;
 		// _greenValue = 0;
@@ -23,33 +23,15 @@ public class PredatorAgent extends Agent {
 			_world.add(new PredatorAgent(_x,_y,_world));
 		}
 	}
-
-	// public void setEnergie(){
-	// 	energie=seuil;
-	// }
-
-	// public int getEnergie(){
-	// 	return energie;
-	// }
 	
 	public void step( )
 	{
 		// met a jour l'agent
-
-		// this.energie--;
 		
 		// A COMPLETER
 
-		// if (Math.random() < p_pred){
-		// 	_world.add(new PredatorAgent(_x,_y,_world));
-		// }
-
 
 		int cellColor[] = _world.getCellState(_x, _y);
-
-		// if (Math.random() < p_pred){
-		// 	_world.add(new PredatorAgent(_x,_y,_world));
-		// }
 
 		
 		cellColor[redId] = 255;
@@ -58,10 +40,49 @@ public class PredatorAgent extends Agent {
 
 		_world.setCellState(_x, _y, cellColor);
 
-		if ( Math.random() > 0.5 ) // au hasard
+		int dist = Integer.MAX_VALUE;
+
+		Agent close_prey = null;
+
+		int r = 5;
+
+		for ( int x2 = _x-r ; x2 <= _x+r ; x2++ ){
+
+			for ( int y2 = _y-r ; y2 <= _y+r ; y2++ ){
+
+				for(Agent j : _world.agents){
+
+					if (j instanceof PreyAgent){
+
+						if ((j._x==x2 && j._y==y2) && (dist > i.distance(j._x,j._y))){
+
+							dist = i.distance(j._x,j._y);
+
+							close_prey = j;
+						}
+					}
+				}
+			}
+		}
+		
+
+		if (close_prey != null){
+
+			int d = Integer.MAX_VALUE;
+			int i = 0;
+
+			while(i<5){
+				
+			}
+
+		}else{
+
+		if( Math.random() > 0.5 ){ // au hasard
 			_orient = (_orient+1) %4;
-		else
+
+		}else{
 			_orient = (_orient-1+4) %4;
+		}
 
 		// met a jour: la position de l'agent (depend de l'orientation)
 		 switch ( _orient ) 
